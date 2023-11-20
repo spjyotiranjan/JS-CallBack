69e518bed589549d86e56cdfed14ac28a1852077
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-11-20 17:10:11 +0530

    Done

--- callback.js
+++ callback.js
@@ -11,3 +11,30 @@ const newCookie = {name:"Biscotti Cookies"};
 
 
 // Progression 5: calling function
+
+// -----------------------------------------------------------------------------------------------------------//
+// Progression 1: create a function to get all the cookies
+function getCookies() {
+  // Progression 2: using setTimeout() - use 1000 units for time parameter
+  setTimeout(()=>{
+    let output = ""
+    cookies.forEach(curr=>{
+      output += `<li>${curr.name}</li>`
+    })
+    document.body.innerHTML = output
+    console.log(output);
+  },1000)
+}
+
+//Progression 3: Create a function to creat cookies
+  // use setTimeout() -- use 2000 units for time parameter
+  function createCookies(callback) {
+    setTimeout(() => {
+      cookies.push(newCookie)
+      callback()
+    }, 2000);
+  }
+
+// Progression 4: calling functions
+createCookies(getCookies)
+// getCookies()
