95f2884dfddc9ff90b1acc1f2f3154772a9ae1ec
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-20 15:48:35 +0530

    Initial commit

---
+++ callback.js
@@ -0,0 +1,13 @@
+const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
+const newCookie = {name:"Biscotti Cookies"};
+
+
+// Progression 1: create a function to get all the cookies
+
+  // Progression 2: using setTimeout() 
+
+//Progression 3: Create a function to creat cookies
+
+
+
+// Progression 5: calling function
---
+++ withoutCallback.js
@@ -0,0 +1,11 @@
+const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
+const newCookie = {name:"Biscotti Cookies"};
+
+// Progression 1: create a function to get all the cookies
+
+  // Progression 2: using setTimeout() - use 1000 units for time parameter
+
+//Progression 3: Create a function to creat cookies
+  // use setTimeout() -- use 2000 units for time parameter
+
+// Progression 4: calling functions
